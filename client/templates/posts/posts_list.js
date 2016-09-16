1f38c1b49000f0cf35fe01bc6b0b248ed4a9d9e5
Tchontk <tchontk@gmail.com>
2016-09-17 00:03:25 +0200

    Commit 4-3 Affichage de collection dans le template `postsList`

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,4 +1,4 @@
-var postsData = [{
+/*var postsData = [{
   title: 'Introducing Telescope',
   url: 'http://sachagreif.com/introducing-telescope/'
 }, {
@@ -10,4 +10,10 @@ var postsData = [{
 }];
 Template.postsList.helpers({
   posts: postsData
+});
+*/
+Template.postsList.helpers({
+  posts: function() {
+    return Posts.find();
+  }
 });
