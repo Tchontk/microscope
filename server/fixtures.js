aaa9d84438cdf48800115ea97b2e8643345815d1
Tchontk <tchontk@gmail.com>
2016-09-18 21:45:22 +0200

    Commit 7-1 Submit post

--- server/fixtures.js
+++ server/fixtures.js
@@ -18,9 +18,4 @@ if (Posts.find().count() === 0) {
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
-
-  Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  });
 }
