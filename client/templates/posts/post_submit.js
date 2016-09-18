ee683e6891a34b93c94f43b0a29d488b800fb392
Tchontk <tchontk@gmail.com>
2016-09-18 23:00:13 +0200

    Commit 7-7 S'assurer de l'unicité de l'url du post.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -10,7 +10,10 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       if (error) {
         return alert(error.reason);
-      }
+      };
+      if (result.postExists) {
+        alert('Ce lien a déjà été utilisé');
+      };
       Router.go('postPage', {
         _id: result._id
       });
