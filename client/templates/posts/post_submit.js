b15848dd04df886e5cb000ca30e948aaf7fafcdf
Tchontk <tchontk@gmail.com>
2016-09-18 23:25:07 +0200

    Commit 8-1 Ajout des formulaire d'édition

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,12 +1,10 @@
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
-
     let post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
-
     Meteor.call('postInsert', post, function(error, result) {
       if (error) {
         return alert(error.reason);
