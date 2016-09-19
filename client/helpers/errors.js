b797180a78c9b49ff19e15c06158de357b696c33
Tchontk <tchontk@gmail.com>
2016-09-19 18:16:55 +0200

    Commit 9-3 Effacer les erreurs après 3 secondes.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,13 +1,26 @@
 Errors = new Meteor.Collection(null);
 
+// function utilisée pour récupérer les messages d'erreurs 
 throwError = function(message) {
   Errors.insert({
     message: message
   });
 };
 
+// Helper du template d'affichage
+// On notera que errors.html comporte deux template
+// _la boucle d'erreurs
+// _le template d'une erreur
 Template.errors.helpers({
   errors: function() {
     return Errors.find();
   }
+});
+
+// Suppresion de l'alert
+Template.error.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function() {
+    Errors.remove(error._id);
+  }, 3000);
 });
