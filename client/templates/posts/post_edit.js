d1c86819ffa2d6aa551b12ec7454c84569b71a07
Tchontk <tchontk@gmail.com>
2016-09-19 18:48:14 +0200

    Commit 9-5 Valider le contenu des posts lors des éditions bien relou...

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -6,6 +6,11 @@ Template.postEdit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url) {
+      return Session.set('postEditErrors', errors);
+    };
     Posts.update(currentPostId, {
       $set: postProperties
     }, function(error) {
@@ -26,5 +31,18 @@ Template.postEdit.events({
       Posts.remove(currentPostId);
       Router.go('postsList');
     };
+  }
+});
+
+Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
   }
 });
