ed213612df003c01e2ebc426161b5683fb630844
Tchontk <tchontk@gmail.com>
2016-09-19 18:44:27 +0200

    Commit 9-4 Validation du contenu du post au moment de la soumission Serveur et Client

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -5,6 +5,13 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    // On appel la fonction de validation des posts
+    var errors = validatePost(post);
+    if (errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    };
+
     Meteor.call('postInsert', post, function(error, result) {
       if (error) {
         return throwError(error.reason);
@@ -16,5 +23,21 @@ Template.postSubmit.events({
         _id: result._id
       });
     });
+  }
+});
+// Supprime les possibles erreurs d'une précédente visite
+// A chaque appel de ce formulaire, l'objet est remis à vide
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+// Gère l'affichage des erreurs
+// _field est le nom du champ
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
   }
 });
