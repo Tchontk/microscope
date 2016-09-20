5d42a76637c867ccc8434243b424afaa42611023
Tchontk <tchontk@gmail.com>
2016-09-20 12:51:22 +0200

    Commit 11-3 _Ajout du filtre sur les notifications _non lu et a destination de l'utilisateur

--- server/publications.js
+++ server/publications.js
@@ -10,7 +10,10 @@ Meteor.publish('comments', function(postId) {
   return Comments.find();
 });
 Meteor.publish('notifications', function() {
-  return Notifications.find({});
+  return Notifications.find({
+    userId: this.userId,
+    read: false
+  });
 });
 /*
 Meteor.publish('posts', function(author) {
