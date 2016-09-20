1b9a4a38eaa584dd243fcd16a710822fdb616b0c
Tchontk <tchontk@gmail.com>
2016-09-20 12:57:39 +0200

    Commit 12-1 Ajout d'une boucle pour la création de 100+ articles

--- server/fixtures.js
+++ server/fixtures.js
@@ -50,4 +50,14 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+  for (var i = 0; i < 100; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000),
+      commentsCount: 0
+    });
+  }
 }
