90f62322053f959e7b142c755b5f4afe5a5fca1e
Tchontk <tchontk@gmail.com>
2016-09-22 18:54:40 +0200

    Commit 13-1 Ajout du bouton vote dans le template Ajout d'un event click .upvote Ajout de la méthode upvote

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,10 +1,17 @@
 Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a');
+  domain: function domain() {
+    const a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
   },
-  ownPost: function() {
+  ownPost: function ownPost() {
     return this.userId === Meteor.userId();
   },
+});
+
+Template.postItem.events({
+  'click .upvote': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id)
+  }
 });
